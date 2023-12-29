02abd55f3149d212df0d246d4f3955fd29543aa8
Eddie Roman <eddie.roman@stsnet.ro>
2023-12-29 18:52:06 +0200

    skeleton

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -48,9 +48,20 @@ export const Sidebar = ({ storageKey = 't-sidebar-state' }: Props) => {
   };
 
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
-    return <Skeleton />;
+    return (
+      <>
+        <div className='flex items-center justify-between mb-2'>
+          <Skeleton className='h-10 w-[50%]' />
+          <Skeleton className='h-10 w-10' />
+        </div>
+        <div className='space-y-2'>
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
+      </>
+    );
   }
-
   return (
     <>
       <div className='font-medium text-xs flex items-center mb-1'>
